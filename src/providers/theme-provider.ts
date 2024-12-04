2dee6cc187afa8d8cae4a0b5b91fbe9d80357505
jasir-pv <jasirahsanpv40@gmail.com>
2024-12-04 21:46:06 +0530

    first commit

---
+++ src/providers/theme-provider.ts
@@ -0,0 +1,13 @@
+// Some comment
+"use client";
+
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+export function ThemeProvider({
+    children,
+    ...props
+  }: React.PropsWithChildren<React.ComponentProps<typeof NextThemesProvider>>) {
+    return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  }
+  
