0fc5e1e5dc8583bfc21ce1056f5ee58f43e4c750
jasir-pv <jasirahsanpv40@gmail.com>
2025-01-21 23:47:29 +0530

    trigger button

--- src/components/global/automations/trigger/keyword.tsx
+++ src/components/global/automations/trigger/keyword.tsx
@@ -1,4 +1,6 @@
 import { useKeywords } from '@/src/hooks/use-automation'
+import { useMutationDataState } from '@/src/hooks/use-mutation-data'
+import { useQueryAutomation } from '@/src/hooks/use-queries'
 import React from 'react'
 
 type Props = {id: string}
@@ -7,10 +9,23 @@ const Keywords = ({ id }: Props) => {
 
     const { onValueChange, keyword, onKeyPress, deleteMutation} = useKeywords(id)
 
-    const 
+    const { latestVariable} = useMutationDataState(['add-keyword'])
+    const { data } = useQueryAutomation(id)
 
   return (
-    <div>Keywords</div>
+    <div className='bg-background-80 flex flex-col gap-y-3 p-3 rounded-xl'>
+        <p className='text-sm text-text-secondary'>
+            add words that trigger automation
+        </p>
+        <div className='flex flex-wrap justify-start gap-2 items-center'>
+        {data?.data?.keywords &&
+         data?.data?.keywords.length > 0 && 
+         data?.data?.keywords.map(
+            (word) =>  word.id!== latestVariable.variables.id && <div>
+                
+            </div> )}
+        </div>
+    </div>
   )
 }
 
